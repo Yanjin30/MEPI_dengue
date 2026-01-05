90f859a69dc31a1cd8e9395f3b82b0b9e52299fe
Erwan Hardy <erwan.hardy@etudiant.univ-rennes.fr>
2026-01-05 12:41:35 +0100

    Create file and add header

---
+++ MEPI_DINGO.R
@@ -0,0 +1,9 @@
+################################
+#~~~HARDY Erwan et FRITZ Leo~~~#
+################################
+
+################################################################################
+# Script pour l'étude d'une épidemie de dengue au Sri Lanka entre 2019 et 2022
+# Prise en compte des migrations inter-urbaines et de la pluviométrie à
+# l'échelle des districts administratifs.
+################################################################################
