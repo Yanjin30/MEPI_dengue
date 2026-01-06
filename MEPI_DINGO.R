44ba3fd602c80995ba2b74cf72cc29ec4ae14a6e
ErwanH <erwan.hardy@etudiant.univ-rennes.fr>
2026-01-06 22:14:08 +0100

    Update MEPI_DINGO.R

--- MEPI_DINGO.R
+++ MEPI_DINGO.R
@@ -3,8 +3,17 @@
 ################################
 
 ################################################################################
-# Script pour l'étude d'une épidemie de dengue au Sri Lanka entre 2019 et 2022
-# Prise en compte des migrations inter-urbaines et de la pluviométrie à
-# l'échelle des districts administratifs.
+# Script pour l'étude d'une épidemie de dengue au Sri Lanka entre 2019 et 2022 #
+# Prise en compte des migrations inter-urbaines et de la pluviométrie à        #
+# l'échelle des districts administratifs.                                      #
 ################################################################################
+setwd("~/Documents/Master/M2/MEPI/projet/MEPI_dengue/datasets_dengo")
 
+# Import 
+# devtools::install_github("GaelBn/BRREWABC")
+library(BRREWABC)
+
+# Import datasets
+data19 <- read.csv(file = "dengue_2019.csv", header = TRUE, sep = ",", dec = ".",)
+data20 <- read.csv(file = "dengue_2020.csv", header = TRUE, sep = ",", dec = ".")
+data21 <- read.csv(file = "dengue_2021.csv", header = TRUE, sep = ",", dec = ".")
