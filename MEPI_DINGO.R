c46d18f00a240aaee64ed53b3ef87a468433a6ed
Erwan Hardy <erwan.hardy@etudiant.univ-rennes.fr>
2026-01-11 18:16:42 +0100

    Make z becaming z(t) in EDO definition

--- MEPI_DINGO.R
+++ MEPI_DINGO.R
@@ -17,39 +17,51 @@ library(BRREWABC)
 library(deSolve)
 
 # Import datasets
-data19 <- read.csv(
+data19 = read.csv(
   file = "dengue_2019.csv",
   header = TRUE,
   sep = ",",
   dec = ".",
 )
 
-data20 <- read.csv(
+data20 = read.csv(
   file = "dengue_2020.csv",
   header = TRUE,
   sep = ",",
   dec = "."
 )
 
-data21 <- read.csv(
+data21 = read.csv(
   file = "dengue_2021.csv",
   header = TRUE,
   sep = ",",
   dec = "."
 )
 
+weather = read.csv(
+  file = "SriLanka_Weather_Dataset_2019_2021.csv",
+  header = TRUE,
+  sep = ",",
+  dec = "."
+)
+
 ################ Déterministe ####################
+z_t <- function(t){
+  zt = runif(n = 1, min = 0, max = 1.5)
+  return(zt)
+}
+
 # Modèle déterministe à base d'ODE
-modele_dengue_deter=function(t,y,param){
+modele_dengue_deter = function(t, y, param) {
   # Définition des paramètres à opimiser
   beta_h = param[1]
   
   # Définition des paramètres fixés
-  z = 0.9
+  z = z_t(t)
   gamma = 1 / 2
   beta_v = 0.375
   mu_v = 1 / 6
-  mu_h= 1 / 75
+  mu_h = 0
   
   # Définition des classes de population
   # Humains
@@ -63,81 +75,112 @@ modele_dengue_deter=function(t,y,param){
   Nv = z * Nh
   
   # Définition des dérivées
-  dIdt = - (gamma + mu_h) * Ih + beta_h * z * Iv  * (Nh - Ih - Rh) / Nh
+  dIdt = -(gamma + mu_h) * Ih + beta_h * z * Iv  * (Nh - Ih - Rh) / Nh
   dRdt = gamma * Ih - mu_h * Rh
-  dVdt = beta_v * (Nv-Iv) * Ih/Nh - mu_v * Iv
+  dVdt = beta_v * (Nv - Iv) * Ih / Nh - mu_v * Iv
   
   return(list(c(dIdt, dRdt, dVdt)))
 }
 
-
 ## Fonction qui réalise une simulation à partir d'un modèle (ODE)
-simulation_deter=function(y,tmax,param,delta_t){
+simulation_deter = function(y, tmax, param, delta_t) {
   # Definition du pas de temps
-  temps=seq(0,tmax,delta_t)
+  temps = seq(0, tmax, delta_t)
   
   # Résolution de l'équation
-  result=ode(y=y,times=temps,func=modele_dengue_deter,parms=param,method="rk4")
+  result = ode(
+    y = y,
+    times = temps,
+    func = modele_dengue_deter,
+    parms = param,
+    method = "rk4"
+  )
   
   # Visualisation
-  par(mfrow=c(1,2))
-  plot(result[,1],result[,3],type="l",col="blue",ylim=c(0,10000))
-  lines(result[,1],result[,2],type="l",col="red")
-  plot(result[,1],result[,4],type="l")
+  par(mfrow = c(1, 2))
+  plot(
+    result[, 1],
+    result[, 3],
+    type = "l",
+    col = "blue",
+    ylim = c(0, 10000)
+  )
+  lines(result[, 1], result[, 2], type = "l", col = "red")
+  plot(result[, 1], result[, 4], type = "l")
   return(result)
 }
-# Juste un test que ça fonctionne correctement, la dynamique ressemble à quelque chose qui me semble logique vu notre modèle
-# Tu me diras si de ton coté aussi
-test = simulation_deter(c(100,100,100),60,c(0.65),1)
+
+test = simulation_deter(c(100, 100, 75), 1500, c(0.65), 1)
+
+
+
+
+
+
+
+
+
+
+
 
 ## Fonction qui prend une dynamique d'infection et de recovery afin d'avoir le nombre d'incidence par mois
-summary_extract=function(vect_inf,vect_recov){
-  monthly_new_case=c()
-  u=1
-  for (i in seq(30,length(vect_inf),30)){
-    if (u==1){
-      monthly_new_case=c(monthly_new_case,sum(diff(c(0,vect_inf[u:i]))+diff(c(0,vect_recov[u:i]))))}
-    if (u!=1){
+summary_extract = function(vect_inf, vect_recov) {
+  monthly_new_case = c()
+  u = 1
+  for (i in seq(30, length(vect_inf), 30)) {
+    if (u == 1) {
+      monthly_new_case = c(monthly_new_case, sum(diff(c(0, vect_inf[u:i])) + diff(c(
+        0, vect_recov[u:i]
+      ))))
+    }
+    if (u != 1) {
       print(u)
-      monthly_new_case=c(monthly_new_case,sum(diff(vect_inf[(u-1):i])+diff(vect_recov[(u-1):i])))}
-    u=i+1}
+      monthly_new_case = c(monthly_new_case, sum(diff(vect_inf[(u - 1):i]) +
+                                                   diff(vect_recov[(u - 1):i])))
+    }
+    u = i + 1
+  }
   return(monthly_new_case)
 }
 
 
 ## Fonction qui calcule la distance à partir d'une trajectoire et de nos statistiques résumées
 ## Prend en entrée les statistiques résumées et un vecteur de paramètre
-distance_deter=function(param,ssobs){
+distance_deter = function(param, ssobs) {
   # Définition des conditions initiales
-  y0=c(100,0,100)
+  y0 = c(100, 0, 100)
   
   # Simulation de la trajectoire
-  simu=simulation_deter(y=y0, tmax=360, param=param, delta_t=1) # On considère grossièrement que chaque mois -> 30 jours
-  infected_dyna=simu[,2]
-  recover_dyna=simu[,3]
+  simu = simulation_deter(
+    y = y0,
+    tmax = 360,
+    param = param,
+    delta_t = 1
+  ) # On considère grossièrement que chaque mois -> 30 jours
+  infected_dyna = simu[, 2]
+  recover_dyna = simu[, 3]
   
   # Création d'un résumé de nos statistiques simulées
-  all_mensual_case = summary_extract(infected_dyna,recover_dyna) # On récupère toutes les nouvelles infection de chaque mois
-  all_mensual_case_obs = rbinom(all_mensual_case,0.05) # Modèle d'observation, hypothèse symptome
+  all_mensual_case = summary_extract(infected_dyna, recover_dyna) # On récupère toutes les nouvelles infection de chaque mois
+  all_mensual_case_obs = rbinom(all_mensual_case, 0.05) # Modèle d'observation, hypothèse symptome
   
   # Comparaison de nos statistiques résumées
-  dist = sum( ( all_mensual_case - ssobs) **2)
+  dist = sum((all_mensual_case - ssobs) ** 2)
   return(c(dist))
 }
 
-
 ############# Stochastique ###############
-modele_dengue_stoch=function(Sh0,Ih0,Rh0,Sv0,Iv0,param,tmax){
-  Sh=Sh0
-  Ih=Ih0
-  Rh=Rh0
-  Nv=2*Sh0 #Nombre de moustique par habitant à définir
-  Iv=0.2*Nv #Proportion de moustique porteur à définir
-  Sv=Nv-Iv
-  betah=param[1]
-  gamma=param[2]
-  betav=param[3]
-  for (i in 1:tmax){
+modele_dengue_stoch = function(Sh0, Ih0, Rh0, Sv0, Iv0, param, tmax) {
+  Sh = Sh0
+  Ih = Ih0
+  Rh = Rh0
+  Nv = 2 * Sh0 #Nombre de moustique par habitant à définir
+  Iv = 0.2 * Nv #Proportion de moustique porteur à définir
+  Sv = Nv - Iv
+  betah = param[1]
+  gamma = param[2]
+  betav = param[3]
+  for (i in 1:tmax) {
     # Définition des taux de transmission
     tx_transmi_h =
       tx_transmi_v =
@@ -158,3 +201,4 @@ modele_dengue_stoch=function(Sh0,Ih0,Rh0,Sv0,Iv0,param,tmax){
     Iv = Iv + new_inf_v
   }
 }
+
